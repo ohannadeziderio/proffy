1969ac0c71eb45ecb3ea912052d05668c49faea6
ohannadeziderio <ohannadeziderio@gmail.com>
2020-09-10 23:51:58 -0300

    [ohannadeziderio] - Aula 03 - Iniciando conexão com o back-end

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
  
 import logoImg from '../../assets/images/logo.svg';
@@ -9,8 +9,17 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 function Landing() {
+    const [totalConnections, setTotalConnectuins] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            console.log(response);
+        })
+    }, []);
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
