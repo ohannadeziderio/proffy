cb47f93c396b89a8899810b540a2536e4a5f5343
ohannadeziderio <ohannadeziderio@gmail.com>
2020-09-13 00:54:41 -0300

    [ohannadeziderio] - Conectando back-end da listagem de professores com o front-end

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,27 +4,38 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string; 
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/18699240?s=60&u=c1ea97a0c7914029ac14a4acd15f6e7b3a9955da&v=4" alt="Ohanna Dezidério"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Ohanna Dezidério</strong>
-                    <span>Francês</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Encantada pela língua francesa.
-                <br /> <br />
-                Apaixonada por idiomas.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora 
-                    <strong>R$ 60,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
 
                 <button type="button">
